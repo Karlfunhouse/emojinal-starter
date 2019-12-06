6ec95e733c74796747af4a02834cf4060870632d
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-12-06 09:46:48 -0700

    Add event listeners and happyMessage function

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ var happy = document.querySelector('.happy');
 var silly = document.querySelector('.silly');
 var crying = document.querySelector('.crying');
 var displayReply = document.querySelector('.message');
+
+
+
+function happyMessage(){
+  console.log('You\'re Happy');
+  displayReply.innerText = 'YAY!'
+  //
+  // Your smile just cheered me up!
+  // You go!
+}
+
+
+
+happy.addEventListener('click', happyMessage);
+silly.addEventListener('click', sillyMessage);
+crying.addEventListener('click', cryingMessage);
