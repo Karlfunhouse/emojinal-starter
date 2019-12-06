3dd712d1c48cb43c2bc43a650c24aec096373e03
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-12-06 09:44:59 -0700

    Defined variables

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+var happy = document.querySelector('.happy');
+var silly = document.querySelector('.silly');
+var crying = document.querySelector('.crying');
+var displayReply = document.querySelector('.message');
