67422d54a5cc8bbeac72a60d8c87369f2c3c6a0e
Karl Nielsen <karl.g.nielsen@gmail.com>
2019-12-06 12:07:50 -0700

    This is as far as I got before going to the review settings

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ var happy = document.querySelector('.happy');
 var silly = document.querySelector('.silly');
 var crying = document.querySelector('.crying');
 var displayReply = document.querySelector('.message');
+var happyWords = ['YAY!', 'Your smile just cheered me up!', 'You go!'];
+var sillyWords = ['Sounds...good?', 'Interesting response', 'Me too.'];
+var cryingWords = ['Do you want to talk?', 'What can I do?', 'Keep your head up.'];
+var randomHappy = happyWords[Math.floor(happyWords.length * Math.random())]
 
 
 
-function happyMessage(){
-  console.log('You\'re Happy');
-  displayReply.innerText = 'YAY!'
+function happyMessage() {
+
+  console.log(happyWords);
+for (var i = 0; i < happyWords.length; i++);{
+    console.log(randomHappy)};
+}
+  // displayReply.innerText = 'YAY!'
   //
   // Your smile just cheered me up!
   // You go!
-}
+
 
 function sillyMessage(){
   console.log("You're silly!")
